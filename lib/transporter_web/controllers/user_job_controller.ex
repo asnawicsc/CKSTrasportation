1b138ad6edb201cd715b5b1c0307c0c629e96102
Damien Lee <yithanglee@gmail.com>
2019-04-09 12:54:34 +0800

    assign user job

--- lib/transporter_web/controllers/user_job_controller.ex
+++ lib/transporter_web/controllers/user_job_controller.ex
@@ -3,6 +3,25 @@ defmodule TransporterWeb.UserJobController do
 
   alias Transporter.Logistic
   alias Transporter.Logistic.UserJob
+  require IEx
+
+  def save_assignment(conn, params) do
+    map_job = params["map_job"] |> Poison.decode!() |> Enum.uniq()
+
+    for job <- map_job do
+      us = Repo.get_by(Logistic.UserJob, job_id: job["job_id"], user_id: job["user_id"])
+
+      if us == nil do
+        job = Map.put(job, "status", "pending accept")
+        a = Logistic.create_user_job(job)
+        IO.inspect(a)
+      end
+    end
+
+    conn
+    |> put_flash(:info, "User job created successfully.")
+    |> redirect(to: user_path(conn, :index))
+  end
 
   def index(conn, _params) do
     user_jobs = Logistic.list_user_jobs()
@@ -20,6 +39,7 @@ defmodule TransporterWeb.UserJobController do
         conn
         |> put_flash(:info, "User job created successfully.")
         |> redirect(to: user_job_path(conn, :show, user_job))
+
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
@@ -44,6 +64,7 @@ defmodule TransporterWeb.UserJobController do
         conn
         |> put_flash(:info, "User job updated successfully.")
         |> redirect(to: user_job_path(conn, :show, user_job))
+
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "edit.html", user_job: user_job, changeset: changeset)
     end
--- lib/transporter_web/router.ex
+++ lib/transporter_web/router.ex
@@ -27,6 +27,7 @@ defmodule TransporterWeb.Router do
     resources("/activities", ActivityController)
     resources("/images", ImageController)
     resources("/user_jobs", UserJobController)
+    post("/user_jobs/save_assignment", UserJobController, :save_assignment)
   end
 
   # Other scopes may use custom stacks.
